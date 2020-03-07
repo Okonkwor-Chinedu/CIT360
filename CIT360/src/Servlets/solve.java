1bb4d2cf08b1f864b6a32c69e1e390a21e859097
okonkwor-chinedu <fra16027@byui.edu>
2020-03-07 14:53:07 -0700

    update

---
+++ CIT360/src/Servlets/solve.java
@@ -0,0 +1,30 @@
+package Servlets;
+
+public class solve extends HttpServlet {
+
+	public static void main(String[] args) {
+		private static final long serialVersionVID =1L;
+		
+		protected void doGet(HttpServletRequest request, HttpServletResponse response)
+		throws ServeletExeption, IOException{
+		// TODO Auto-generated method stub
+			PrintWriter out = response.getWriter();
+			int n1 = Integer.parseInt(request.getParameter("num1"));
+			int n2 = Integer.parseInt(request.getParameter("num2"));
+			String operation = request.getParameter("operation");
+			int result = 0;
+
+			switch(operation) {
+			case"+":
+			result = n1 + n2;
+			break;
+
+			case"-":
+			result = n1 - n2;
+
+			default:
+				break;
+			}
+			out.println(result);
+			}
+	}
