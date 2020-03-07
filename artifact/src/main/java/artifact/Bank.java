c74b6c4c09b13a61644583a698834f4e97e9806d
okonkwor-chinedu <fra16027@byui.edu>
2020-03-07 16:02:43 -0700

    u

---
+++ artifact/src/main/java/artifact/Bank.java
@@ -0,0 +1,34 @@
+import org.hibernate.annotation.GenericGenerator;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "banks_table")
+public class Bank{
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(generator = "incrementor")
+	@GenericGenarator(name = "incrementator", strategy = "increment" )
+	private int id;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	@Column(name = "name")
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
