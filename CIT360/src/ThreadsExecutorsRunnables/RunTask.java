ec0cdb8906a67bf31a30dcab3404f790c90fac3f
okonkwor-chinedu <fra16027@byui.edu>
2020-03-12 23:27:37 -0700

    UPDATE

--- CIT360/src/ThreadsExecutorsRunnables/RunTask.java
+++ CIT360/src/ThreadsExecutorsRunnables/RunTask.java
@@ -20,7 +20,7 @@ public class RunTask implements Runnable {
 
 
 
-        for(int i = 0; i < 50; i++){
+        for(int i = 0; i < 10; i++){
 
             System.out.println("Running " + Thread.currentThread().getName());
 
